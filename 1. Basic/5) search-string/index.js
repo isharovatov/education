97104ffdec43b874ba7e8d36dbc509ad10f4a3f0
isharovatov <sharovatov.exceedteam@gmail.com>
2021-06-21 18:25:48 +0300

    Search string

--- 1. Basic/5) search-string/index.js
+++ 1. Basic/5) search-string/index.js
@@ -3,7 +3,6 @@ function searchString(obj) {
   for (var key in obj) {
     if (obj[key] !== "" && obj[key] !== null && obj[key] !== undefined) {
       str += key + "=" + obj[key] + "&";
-      console.log(str);
     }
   }
   str = str.substring(0, str.length - 1);
