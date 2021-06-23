413deb6986de25af5f82a425a6ea6cad49cc341c
isharovatov <sharovatov.exceedteam@gmail.com>
2021-06-23 10:35:57 +0300

    is equal simple

--- 2. Data Types/3) is-equal-simple/index.js
+++ 2. Data Types/3) is-equal-simple/index.js
@@ -1,5 +1,37 @@
 function isEqual(a, b) {
-  // Напиши свой код здесь
+  console.log(null === {});
+  let FristMassivKey = [];
+  let SecondMassivKey = [];
+
+  let FristMassivValues = [];
+  let SecondMassivValues = [];
+
+  try {
+    FristMassivKey = Object.keys(a);
+    SecondMassivKey = Object.keys(b);
+
+    FristMassivValues = Object.values(a);
+    SecondMassivValues = Object.values(b);
+  } catch {
+    return false;
+  }
+
+  if (FristMassivKey.length !== SecondMassivKey.length) {
+    return false;
+  }
+  for (let i = 0; i <= FristMassivKey.length - 1; i++) {
+    if (!SecondMassivKey.includes(FristMassivKey[i])) {
+      return false;
+    }
+  }
+  for (let i = 0; i <= FristMassivKey.length - 1; i++) {
+    let j = SecondMassivKey.indexOf(FristMassivKey[i]);
+
+    if (FristMassivValues[i] !== SecondMassivValues[j]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 window.isEqual = isEqual;
