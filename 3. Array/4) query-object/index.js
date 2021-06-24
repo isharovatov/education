a40d2bd360bed2d3daf828211c00b6f5b1d7aff8
isharovatov <sharovatov.exceedteam@gmail.com>
2021-06-24 15:36:34 +0300

    query to object fixes

--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -2,24 +2,19 @@ import stringToType from "./../../2. Data Types/1) string-to-type/index.js";
 
 function queryToObject(query) {
   let queryObj = new Object();
-  if (query === "" || query === "?") {
-    return queryObj;
-  } else {
-    let str
-    str = query.split("?"); //.join()
-    str = str[1].split("&");
+  let str;
+  str = query.split(/\?|\&/); //.join()
 
-    str.forEach((pairPropertyValue) => {
+  str.forEach((pairPropertyValue) => {
+    console.log(pairPropertyValue);
+    if (pairPropertyValue != "") {
       pairPropertyValue = pairPropertyValue.split("=");
       queryObj[pairPropertyValue[0]] = stringToType(pairPropertyValue[1]);
-    });
+    }
+  });
 
-    return queryObj;
-  }
+  return queryObj;
 }
-
-
-
 
 window.queryToObject = queryToObject;
 
