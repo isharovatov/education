ddf41aad255d734e17bd538b64540c5dc1e67875
isharovatov <sharovatov.exceedteam@gmail.com>
2021-06-24 10:17:22 +0300

    array sort without apply

--- 3. Array/3) arrays-sort/index.js
+++ 3. Array/3) arrays-sort/index.js
@@ -1,6 +1,6 @@
 function arraysSort(arr) {
   arr.sort(function (a, b) {
-    if (Math.max.apply(Math, a) < Math.max.apply(Math, b)) {
+    if (Math.max(...a) < Math.max(...b)) {
       return -1;
     }
   });
