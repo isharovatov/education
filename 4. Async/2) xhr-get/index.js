3525708f34db06eac3b92b20f2ec45f792a6064b
isharovatov <sharovatov.exceedteam@gmail.com>
2021-06-25 17:42:13 +0300

    get inquiry

--- 4. Async/2) xhr-get/index.js
+++ 4. Async/2) xhr-get/index.js
@@ -1,5 +1,23 @@
 function xhrGet(url) {
-  // Напиши свой код здесь
+  return new Promise(function (resolve, reject) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("GET", url);
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onload = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status < 300 && xhr.status > 199) {
+          resolve(JSON.parse(xhr.response));
+          console.log(JSON.parse(xhr.response));
+          console.log(xhr.response);
+        } else {
+          reject(JSON.parse(xhr.response));
+        }
+      }
+    };
+    xhr.send();
+  });
 }
 
 window.xhrGet = xhrGet;
