b46dcadd8bc8c012a2d04f4431fe6823a27dd9fe
isharovatov <sharovatov.exceedteam@gmail.com>
2021-06-25 17:52:13 +0300

    fix

--- 4. Async/2) xhr-get/index.js
+++ 4. Async/2) xhr-get/index.js
@@ -9,8 +9,6 @@ function xhrGet(url) {
       if (xhr.readyState === 4) {
         if (xhr.status < 300 && xhr.status > 199) {
           resolve(JSON.parse(xhr.response));
-          console.log(JSON.parse(xhr.response));
-          console.log(xhr.response);
         } else {
           reject(JSON.parse(xhr.response));
         }
