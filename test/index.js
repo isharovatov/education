f5cea5226690e0ccb173989171de01713764acbf
isharovatov <sharovatov.exceedteam@gmail.com>
2021-06-29 13:48:48 +0300

    first fixes

--- test/index.js
+++ test/index.js
@@ -2,22 +2,13 @@ let count = 0;
 
 function addinput() {
   count += 1;
-  var val = document.getElementById("elem"+count).value;
-  if (val == "555") {
-    var newDiv = document.createElement("div");
-    newDiv.innerHTML =
-      "<input id = 'elem" + (count+1) + "'  type='text' value='---'>";
-    my_div = document.getElementById("container").appendChild(newDiv);
-  } else if (count % 2) {
-    var newDiv = document.createElement("div");
-    newDiv.innerHTML =
-      "<input id = 'elem" + (count+1) + "'  type='text' value='" + val.toLowerCase() + "'>";
-    my_div = document.getElementById("container").appendChild(newDiv);
-  } else {
-    var newDiv = document.createElement("div");
-    newDiv.innerHTML =
-      "<input id = 'elem" + (count+1) + "'  type='text' value='" + val.toUpperCase() + "'>";
-    my_div = document.getElementById("container").appendChild(newDiv);
-  }
+  let val = document.getElementById("elem" + count).value;
+  let newDiv = document.createElement("input");
+  newDiv.id = "elem" + (count + 1);
+  newDiv.type = "text";
+  let valueInput = count % 2 ? val.toUpperCase() : val.toLowerCase();
+  if (val === "555") valueInput = "---";
+  newDiv.value = valueInput;
+  my_div = document.getElementById("container").appendChild(newDiv);
 }
 add.onclick = addinput;
